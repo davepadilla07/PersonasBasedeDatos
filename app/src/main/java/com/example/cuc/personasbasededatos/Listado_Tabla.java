9da53bffefd975f851bef1dc40e650f81c923340
david <ing.dave@hotmail.es>
2017-05-13 11:59:33 -0500

    datos no terminada

--- app/src/main/java/com/example/cuc/personasbasededatos/Listado_Tabla.java
+++ app/src/main/java/com/example/cuc/personasbasededatos/Listado_Tabla.java
@@ -2,12 +2,22 @@ package com.example.cuc.personasbasededatos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+import java.util.ArrayList;
 
 public class Listado_Tabla extends AppCompatActivity {
+    private TableLayout tabla;
+    private ArrayList<Persona> personas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado__tabla);
+
+        tabla=(TableLayout)findViewById(R.id.tblPersonas);
+
+        personas=Datos.traerPersonas(getApplicationContext());
+
     }
 }
